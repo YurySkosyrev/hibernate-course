f752521590d0f37e2c053c6b1f4d5018ccef79c7
YurySkosyrev <skosyrev-87@mail.ru>
2023-05-06 22:59:41 +0300

    Step 1.5/14 part 2

--- hibernate-starter/src/main/java/com/dmdev/HibernateRunner.java
+++ hibernate-starter/src/main/java/com/dmdev/HibernateRunner.java
@@ -1,8 +1,11 @@
 package com.dmdev;
 
+import com.dmdev.entity.User;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+
+import java.time.LocalDate;
 
 public class HibernateRunner {
     public static void main(String[] args) {
@@ -11,7 +14,19 @@ public class HibernateRunner {
 
         try (SessionFactory sessionFactory = configuration.buildSessionFactory();
              Session session = sessionFactory.openSession()) {
-            System.out.println("OK");
+
+            session.beginTransaction();
+
+            User user = User.builder()
+                    .username("ivan@mail.ru")
+                    .firstname("Ivan")
+                    .lastname("Ivanov")
+                    .birthDate(LocalDate.of(2000, 1, 12))
+                    .age(20)
+                    .build();
+
+            session.save(user);
+            session.getTransaction().commit();
         }
     }
 }
